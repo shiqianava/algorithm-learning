e525acea1fb42cd1dbe76d21fbadf865e69e18d8
Sqava <1085503167@qq.com>
2022-09-08 22:18:34 +0800

    2022.9.8

---
+++ codes/3-insertion-sort.js
@@ -0,0 +1,24 @@
+const arr = [45, 36, 12, 89, 37, 56, 10, 123, 20, 563, 3, 22, 1];
+
+function insertionSort(arr) {
+    if (arr == null || arr.length < 2) {
+        return;
+    }
+
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = i - 1; j >= 0; j--) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+}
+
+function swap(arr, i, j) {
+    let tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+}
+
+insertionSort(arr);
+console.log(arr);
